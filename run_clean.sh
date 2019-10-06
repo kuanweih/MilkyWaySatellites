88c5f2409ee83b1e7e31b712a7dc85258a30784a
kuanweih <kuanweih@andrew.cmu.edu>
2019-10-06 17:56:16 -0400

    kept the csv

--- run_clean.sh
+++ run_clean.sh
@@ -1,5 +1,5 @@
 
 
-rm  -rf  ori-dwarfs/*.npy  ori-dwarfs/*.csv  ori-dwarfs/*.txt
+rm  -rf  ori-dwarfs/*.npy  ori-dwarfs/*.txt
 rm  -rf  ori-dwarfs/more/*.npy  ori-dwarfs/more/*.txt
 rm  -rf  ori-dwarfs/McConnachie/*dwarfs*
