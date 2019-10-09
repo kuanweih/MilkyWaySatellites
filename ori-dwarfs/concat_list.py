34d2eba4188155214a2b4477d59e13fc0b2ab9cb
kuanweih <kuanweih@andrew.cmu.edu>
2019-10-08 22:00:44 -0400

    added rh_pc

--- ori-dwarfs/concat_list.py
+++ ori-dwarfs/concat_list.py
@@ -2,11 +2,10 @@ import numpy as np
 import pandas as pd
 
 from typing import Dict
-# from src.tools import get_dic_list_npy, dist2
-# from src.param_patch_candidate import PATCH_DIST, N_PATCH_MAX
 
 
-def get_dic_list_npy(path: str, quantitys: str):
+
+def get_dic_list_npy(path: str, quantitys: str) -> Dict:
     """ Select a dict from a numpy array based on keys as quantitys
     : path : path of the dict npy file
     : quantitys : target keys
@@ -31,6 +30,9 @@ if __name__ == '__main__':
     dict2 = get_dic_list_npy(path_more, quantitys)
 
     dict_joint = {q: np.concatenate((dict1[q], dict2[q])) for q in quantitys}
+
+    dict_joint['rh_pc'] = dict_joint['rh(arcmins)'] / 60.
+    dict_joint['rh_pc'] *= 2. * np.pi / 360. * dict_joint['Distance_pc']
 
     print('Saving txt files and npy files\n')
     np.save("dwarfs-joint", dict_joint)
